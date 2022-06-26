365f6ab7f4828548cd3c751470c349c02a234036
shivam <shivamsisodia8656816@gmail.com>
2022-06-27 00:50:11 +0530

    updating app.js and index.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
 import CardDetails from "./components/CardDetails";
 import Cards from "./components/Cards";
+
 function App() {
   return (
-    <div className='App'>
-      <Header></Header>
+    <>
+      <Header />
       <Routes>
         <Route path='/' element={<Cards />} />
         <Route path='/cart/:id' element={<CardDetails />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import store from "./store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </BrowserRouter>
-    ;
-  </Provider>
+  </Provider>,
+  document.getElementById("root")
 );
